1dace6e66da963c86790870905a427dac686d825
Alexander Kostin <asurv2@gmail.com>
2022-07-28 22:51:07 +0300

    bugfix

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,17 +1,15 @@
 const express = require("express");
 
 const { basedir } = global;
-
 const ctrl = require(`${basedir}/controllers/auth`);
-
-const { ctrlWrapper } = require(`${basedir}/helpers`);
+const { ctrlWrapper } = require(`${basedir}/middlewares`);
 
 const router = express.Router();
 
-// signup
+// router.post("/signup")
 router.post("/register", ctrlWrapper(ctrl.register));
 
-// signin
+// router.post("/signin")
 router.post("/login", ctrlWrapper(ctrl.login));
 
 module.exports = router;
