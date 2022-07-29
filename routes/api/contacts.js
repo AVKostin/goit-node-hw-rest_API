1974819279993c28a378d4ad71a04f023ee718e9
Alexander Kostin <asurv2@gmail.com>
2022-07-29 13:29:33 +0300

    add auth

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,22 +1,25 @@
 const express = require("express");
 
 const { basedir } = global;
-const { ctrlWrapper } = require(`${basedir}/middlewares`);
+const { auth, ctrlWrapper } = require(`${basedir}/middlewares`);
 const { contacts: ctrl } = require(`${basedir}/controllers`);
 
 const router = express.Router();
 
-router.get("/", ctrlWrapper(ctrl.getAll));
+router.get("/", auth, ctrlWrapper(ctrl.getAll));
 
-router.get("/:contactId", ctrlWrapper(ctrl.getById));
+router.get("/:contactId", auth, ctrlWrapper(ctrl.getById));
 
-router.post("/", ctrlWrapper(ctrl.addContact));
+router.post("/", auth, ctrlWrapper(ctrl.addContact));
 
-router.put("/:contactId", ctrlWrapper(ctrl.updateContact));
+router.put("/:contactId", auth, ctrlWrapper(ctrl.updateContact));
 
 router.patch(
-    "/:contactId/favorite", ctrlWrapper(ctrl.updateStatusContact));
+    "/:contactId/favorite",
+    auth,
+    ctrlWrapper(ctrl.updateStatusContact),
+);
 
-router.delete("/:contactId", ctrlWrapper(ctrl.removeContact));
+router.delete("/:contactId", auth, ctrlWrapper(ctrl.removeContact));
 
 module.exports = router;
