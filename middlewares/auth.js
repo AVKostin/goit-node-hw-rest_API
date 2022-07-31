f4faf403f9e36f5b9e40d72b4a00ec33901c91be
Alexander Kostin <asurv2@gmail.com>
2022-07-31 21:53:23 +0300

    del comments

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,37 +1,3 @@
-// const jwt = require("jsonwebtoken");
-
-// const { basedir } = global;
-
-// const { User } = require(`${basedir}/models/user`);
-
-// const { createError } = require(`${basedir}/helpers`);
-
-// const { SECRET_KEY } = process.env;
-
-// const auth = async (req, res, next) => {
-//     const { authorization = "" } = req.headers;
-//     const [bearer, token] = authorization.split(" ");
-//     try {
-//         if (bearer !== "Bearer") {
-//             throw createError(401, "Not authorized");
-//         }
-//         const { id } = jwt.verify(token, SECRET_KEY);
-//         const user = await User.findById(id);
-//         if (!user || !user.token) {
-//             throw createError(401, "Not authorized");
-//         }
-//         req.user = user;
-//         next();
-//     } catch (error) {
-//         if (error.message === "Invalid signature") {
-//             error.status = 401;
-//         }
-//         next(error);
-//     }
-// };
-
-// module.exports = auth;
-
 const jwt = require("jsonwebtoken");
 
 const { basedir } = global;
@@ -47,14 +13,14 @@ const { SECRET_KEY } = process.env;
 const auth = async (req, _, next) => {
     const { authorization = "" } = req.headers;
     const [bearer, token] = authorization.split(" ");
-    if (bearer !== "Bearer") {
-        next(createError(401));
-    }
     try {
-        const { contactId } = jwt.verify(token, SECRET_KEY);
-        const user = await User.findById(contactId);
+        if (bearer !== "Bearer") {
+            next(createError(401, "Not Bearer"));
+        }
+        const { id } = jwt.verify(token, SECRET_KEY);
+        const user = await User.findById(id);
         if (!user || !user.token) {
-            next(createError(401));
+            next(createError(401, "Not Valid token"));
         }
         req.user = user;
         next();
--- middlewares/index.js
+++ middlewares/index.js
@@ -3,11 +3,3 @@ const auth = require("./auth");
 module.exports = {
     auth,
 };
-
-// const validation = require("./validation");
-// const ctrlWrapper = require("./ctrlWrapper");
-
-// module.exports = {
-//     validation,
-//     ctrlWrapper,
-// };
