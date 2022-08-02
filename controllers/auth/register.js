8235d4038d323bd8936c6dee02e9b93758356a29
Alexander Kostin <asurv2@gmail.com>
2022-08-02 19:55:44 +0300

    add avatar

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -1,4 +1,5 @@
 const bcrypt = require("bcryptjs");
+const gravatar = require("gravatar");
 
 const { basedir } = global;
 
@@ -17,8 +18,12 @@ const register = async (req, res) => {
         throw createError(409, `${email} is already exist`);
     }
     const hashPassword = await bcrypt.hash(password, 10);
-
-    const result = await User.create({ ...req.body, password: hashPassword });
+    const avatarURL = gravatar.url(email);
+    const result = await User.create({
+        ...req.body,
+        password: hashPassword,
+        avatarURL,
+    });
     res.status(201).json({
         name: result.name,
         email: result.email,
--- models/user.js
+++ models/user.js
@@ -20,6 +20,10 @@ const userSchema = Schema(
             minlength: 6,
             required: true,
         },
+        avatarURL: {
+            type: String,
+            required: true,
+        },
         token: {
             type: String,
         },
